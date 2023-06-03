d14c3f9e89070bb6372e2434888950ec7213e46f
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-06-04 01:44:27 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import mongoose from "mongoose"
-import app from "./app";
-import config from "./config";
-const port = process.env.PORT || 3000
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
-   try {
-     await mongoose.connect(config.database_url as string);
-     console.log(`Database Connected Successfully`)
-     app.listen(config.port, () => {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database Connected Successfully`)
+    app.listen(config.port, () => {
       console.log(`Application listening on port ${config.port} `)
     })
-   } catch (error) {
-    console.log(`Failed to connect`,error)
-   }
-
+  } catch (error) {
+    console.log(`Failed to connect`, error)
   }
+}
 
-  main()
+main()
